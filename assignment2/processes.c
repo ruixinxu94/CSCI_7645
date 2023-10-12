d885194b8f597ae75313625d134590cf9a00da66
Xu Ruixin <dxu26@volvocars.com>
2023-10-12 13:40:02 -0700

    assignment2 updated

--- assignment2/processes.c
+++ assignment2/processes.c
@@ -24,7 +24,7 @@ int createChildProcesses(int processSize) {
 }
 
 int main(int argc, char **argv) {
-    if (argc < 2 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "--h") == 0) {
+    if (argc < 2 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
         printf("Usage: ./processes <n>\n");
         exit(EXIT_FAILURE);
     }

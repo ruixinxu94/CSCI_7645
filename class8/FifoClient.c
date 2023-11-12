994e939f681dd8b33bc40a29e7f9698d8a31663a
Xu Ruixin <dxu26@volvocars.com>
2023-11-11 22:05:07 -0800

    class8 updated

--- class8/FifoClient.c
+++ class8/FifoClient.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 
-#define SERVER_FIFO_NAME "RequestFifo"
-#define CLIENT_FIFO_TEMPLATE "ResponsFifo.%d"
-#define MAX_LENGTH 100
+/* Define the protocol */
 
 typedef struct {
     pid_t clientId;
@@ -20,76 +18,112 @@ typedef struct {
     unsigned lastTicket;
 } Response;
 
-int main(int argc, char **argv) {
-    int numTickets, requestFifoDescriptor, responseFifoDescriptor;
+/* FIFO names */
+
+#define SERVER_FIFO_NAME "RequestFifo"
+#define CLIENT_FIFO_TEMPLATE "ResponseFifo.%d"
+
+#define MAX_LENGTH 100
+
+/* Create exit handler for removing response FIFO */
+void removeResponseFifo() {
+    int status;
+    char responseFifoName[MAX_LENGTH];
+
+    snprintf(responseFifoName, MAX_LENGTH, CLIENT_FIFO_TEMPLATE, getpid());
+
+    status = unlink(responseFifoName);
+    if (status == -1) {
+        printf("Client failed to remove response FIFO.\n");
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+    int numTickets, status, requestFifoDescriptor, responseFifoDescriptor;
     Request request;
     Response response;
     int numRead, numWritten;
     char responseFifoName[MAX_LENGTH];
 
-    if (argc != 2) {
-        printf("Usage: %s <number_of_tickets>\n", argv[0]);
+    if (argc != 2 || strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+        printf("Usage: %s <number of tickets>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     numTickets = atoi(argv[1]);
     if (numTickets <= 0) {
-        printf("Number of tickets must be positive\n");
+        printf("Number of tickets must be positive.\n");
         exit(EXIT_FAILURE);
     }
 
-    // Prepare the request
+    /* Register the exit handler */
+    status = atexit(removeResponseFifo);
+    if (status != 0) {
+        printf("Client was unable to register exit handler.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Create the response FIFO */
+    snprintf(responseFifoName, MAX_LENGTH, CLIENT_FIFO_TEMPLATE, getpid());
+
+    status = mkfifo(
+            responseFifoName,
+            S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP
+    );
+    if (status == -1) {
+        printf("Client failed to create response FIFO.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Prepare the request */
     request.clientId = getpid();
     request.numTickets = (unsigned) numTickets;
 
-    // Create the response FIFO name
-    snprintf(responseFifoName, MAX_LENGTH, CLIENT_FIFO_TEMPLATE, request.clientId);
-
-    // Create the response FIFO
-    if (mkfifo(responseFifoName, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP) == -1) {
-        perror("Client failed to create response FIFO");
-        exit(EXIT_FAILURE);
-    }
-
-    // Put the request on the request FIFO
+    /* Put the request on the request FIFO */
     requestFifoDescriptor = open(SERVER_FIFO_NAME, O_WRONLY);
     if (requestFifoDescriptor == -1) {
-        perror("Cannot reach the server");
-        unlink(responseFifoName); // Clean up FIFO
+        printf("Client cannot reach server.\n");
         exit(EXIT_FAILURE);
     }
 
-    numWritten = write(requestFifoDescriptor, &request, sizeof(request));
-    if (numWritten != sizeof(request)) {
-        perror("Client encountered partial write error while writing to request FIFO");
-        close(requestFifoDescriptor);
-        unlink(responseFifoName); // Clean up FIFO
+    numWritten = write(requestFifoDescriptor, &request, sizeof(Request));
+    if (numWritten < sizeof(Request)) {
+        printf("Client encountered partial write error while writing the request.\n");
         exit(EXIT_FAILURE);
     }
-    close(requestFifoDescriptor); // Request sent, can close the descriptor
 
-    // Open the response FIFO for reading
+    status = close(requestFifoDescriptor);
+    if (status == -1) {
+        printf("Client failed to close the request FIFO.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Read the response */
     responseFifoDescriptor = open(responseFifoName, O_RDONLY);
     if (responseFifoDescriptor == -1) {
-        perror("Client failed to open response FIFO");
-        unlink(responseFifoName); // Clean up FIFO
+        printf("Client failed to open response FIFO.\n");
         exit(EXIT_FAILURE);
     }
 
-    // Read the response
-    numRead = read(responseFifoDescriptor, &response, sizeof(response));
-    if (numRead != sizeof(response)) {
-        perror("Client failed to read full response from FIFO");
-    } else {
-        printf("First ticket = %u, last ticket = %u\n", response.firstTicket, response.lastTicket);
-    }
-
-    // Cleanup: Close FIFO and delete it
-    close(responseFifoDescriptor);
-    if (unlink(responseFifoName) == -1) {
-        perror("Client failed to delete response FIFO");
+    numRead = read(responseFifoDescriptor, &response, sizeof(Response));
+    if (numRead == -1) {
+        printf("Client failed to read response.\n");
         exit(EXIT_FAILURE);
     }
 
-    exit(EXIT_SUCCESS); // Corrected the exit call with the appropriate argument
+    printf(
+            "First ticket = %u, last ticket = %u\n",
+            response.firstTicket,
+            response.lastTicket
+    );
+
+    status = close(responseFifoDescriptor);
+    if (status == -1) {
+        printf("Client failed to close response FIFO.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    exit(EXIT_SUCCESS);
+
 }
